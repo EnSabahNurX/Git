0b1930d3cff8005865547d5cee390d5f11061263
Ricardo Almeida <ricklimadealmeida@gmail.com>
2022-05-21 15:57:55 +0100

    averafe program using floating size

---
+++ students-score.py
@@ -0,0 +1,4 @@
+score1 = float(input('First score:'))
+score2 = float(input('Second score:'))
+average = (score1 + score2) / 2
+print(f'The average score between {score1:.1f} and {score2:.1f} is {average:.2f}.')
