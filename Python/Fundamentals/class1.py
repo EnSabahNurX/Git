3d5a351d4421a303ce801d6d76153f814615fe3e
EnSabahNurX <ricklimadealmeida@gmail.com>
2022-06-12 03:26:27 +0100

    print function and variables

---
+++ Python/Fundamentals/class1.py
@@ -0,0 +1,17 @@
+# print('Hello, World!')
+# print('The sum of 5 + 5 is:', 5+5)
+# print('Number as string:', '5' + '5')
+# print(-5.55*2)
+# print('-5.55'*2)
+# print('Outside quotes and "inside quotes".')
+
+# print(f'The sum of 2 + 2 is: {2+2}')
+# print(f'Concactenance of 2 with 2 is: {"2"+"2"}')
+# print(f'The product of 2 with 2 is: {2*2}')
+# print(f'Repeat "A" 5 times: {"A"*5}')
+# print(f'Float division of 10 by 3 is: {10/3}')
+# print(f'Whole division of 10 by 3 is: {10//3}')
+# print(f'Module of 10 by 3 is: {10%3}')
+# print(f'Exponential of 2 to 20 is: {2**20}')
+# print(f'Changing the priority of (5+2)*10: {(5+2)*10}')
+
